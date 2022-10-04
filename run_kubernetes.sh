a7af69edcfd7da783eaf548714c3b99771828659
Fikri Helmi Setiawan <fikrihelmi17@gmail.com>
2022-10-04 13:11:55 +0700

    initial commit

---
+++ run_kubernetes.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath="fikrihelmi17/udacity-ml"
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run udacity-ml --generator=run-pod/v1 --image=$dockerpath --port=80 --labels app=udacity-ml
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward udacity-ml 8000:80
+
