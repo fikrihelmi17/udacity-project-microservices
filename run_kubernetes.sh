c4611f1827b3c20926c65568089dd4859da1fa48
Fikri Helmi Setiawan <fikrihelmi17@gmail.com>
2022-10-09 06:33:38 +0700

    fix the code

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,7 +8,7 @@ dockerpath=fikrihelmi17/udacity-project-ml
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run udacity-project-ml --image=$dockerpath --port=80 --labels app=udacity-project-ml
+kubectl run udacity-project-ml --image=fikrihelmi17/udacity-project-ml --port=80 --labels app=udacity-project-ml
 
 # Step 3:
 # List kubernetes pods
@@ -16,5 +16,5 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl -- port-forward udacity-project-ml 8000:80
+kubectl port-forward udacity-project-ml 8000:80
 
